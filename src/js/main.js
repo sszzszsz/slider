08ab773cb9d7fcd167d3694cae08655f285735a8
Suzu Nagano <sszz12121994@gmail.com>
2020-02-04 16:11:31 +0900

    [fix]JS作成中

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-import {hello} from './_sub';
+import { hello } from './_sub';
 
 const message = 'Hello World';
 
 // sub.jsに定義されたJavaScriptを実行する。
 hello(message);
+
+
+import MerryGoRound from './_sub';
+let Slider1 = new MerryGoRound({
+	el: 'slider',
+	indicator: true,
+	count: 1,
+	slideSpeed: 3000
+});
